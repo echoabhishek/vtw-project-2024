f3a92440ad98949bd6a961016e70033b7a0e0f68
Nam Mai <maithenamanh@gmail.com>
2024-09-14 16:14:32 -0700

    Temp filter to make filtering location works on map

--- vtw-ui/src/components/banner/Banner.tsx
+++ vtw-ui/src/components/banner/Banner.tsx
@@ -20,21 +20,10 @@ const Banner: React.FC = () => {
     <div className='border border-[#333333] bg-[#f3fe74]'>
       <div className="flex justify-evenly items-center">
         <h1 className="font-extrabold text-[55px] text-[#333333]">
-            vanquest
+            [van]quest
         </h1>
         <div className="flex flex-col items-center p-4 space-x-4">
             <SearchBar onSearch={handleSearch}/>
-            {/* Filters */}
-            {/* <select 
-            value={selectedFilter} 
-            onChange={handleFilterChange}
-            className="ml-4 border border-gray-300 p-2 rounded"
-            >
-            <option value="all">All</option>
-            <option value="events">Events</option>
-            <option value="users">Users</option>
-            <option value="locations">Locations</option>
-            </select> */}
             <Filters/>   
         </div>
       </div>
