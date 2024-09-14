d836b25d9a9b8c213e5f32ba505cc2f7740d039c
Mark Pettersson <markp@evidentpoint.com>
2024-09-14 14:48:47 -0700

    add nav bar

--- vtw-ui/src/App.tsx
+++ vtw-ui/src/App.tsx
@@ -1,5 +1,5 @@
 import React, { useState } from 'react';
-import { BrowserRouter as Router, Route, Routes, Navigate } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Routes, Navigate, Link } from 'react-router-dom';
 import Login from './pages/SignIn/index';
 import MapComponent from './components/map/MapComponent';
 import EventPage from './pages/Events/EventPage';
@@ -17,7 +17,7 @@ const App = () => {
 
   return (
     <Router>
-      <div className="min-h-screen bg-gray-100">
+      <div className="min-h-screen bg-gray-100 relative">
         <Routes>
           <Route 
             path="/" 
@@ -62,6 +62,16 @@ const App = () => {
             } 
           />
         </Routes>
+
+        {isAuthenticated && (
+          <nav className="absolute bottom-1/4 left-1/2 transform -translate-x-1/2 bg-[#F3FE74] rounded-full px-6 py-3">
+            <ul className="flex space-x-6">
+              <li><Link to="/map" className="text-black hover:text-gray-700">World</Link></li>
+              <li><Link to="/quests" className="text-black hover:text-gray-700">My Quests</Link></li>
+              <li><Link to="/leaderboard" className="text-black hover:text-gray-700">Leaderboard</Link></li>
+            </ul>
+          </nav>
+        )}
       </div>
     </Router>
   );
---
+++ vtw-ui/src/pages/MyQuests/MyQuests.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const MyQuests = () => {
+  return (
+    <div>
+      <h1>My Quests</h1>
+      {/* Add your quests content here */}
+    </div>
+  );
+};
+
+export default MyQuests;
