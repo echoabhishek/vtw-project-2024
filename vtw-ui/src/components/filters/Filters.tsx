dff4919b25ac4515446344a411f4823aeb9c3050
Abhishek Arora <mail2abhishekarora@gmail.com>
2024-09-14 19:26:22 -0700

    added useCallback for filtering

--- vtw-ui/src/components/filters/Filters.tsx
+++ vtw-ui/src/components/filters/Filters.tsx
@@ -1,6 +1,6 @@
 // src/components/Filters.tsx
 
-import React, { useState } from 'react';
+import React, { useCallback, useState } from 'react';
 import ProductFilterIcon from '../icons/ProductFilterIcon';
 import DesignFilterIcon from '../icons/DesignFilterIcon';
 
@@ -23,26 +23,29 @@ const Filters: React.FC<FiltersProps> = ({ selectedCategory, onFilterChange }) =
         }
     };
 
-    // const filterStyles = {
-    //     DESIGN: "bg-[#ff9cf5]",
-    //     PRODUCT: "bg-[#afbeff]",
-    //     DEV: "bg-[#bfadf4]",
-    //     TRENDING: "bg-[#fbabb6]",
-    //     MENTORSHIP: "bg-[#95f9ab]",
-    // };
+    const filterStyles = {
+        DESIGN: "bg-[#ff9cf5]",
+        PRODUCT: "bg-[#afbeff]",
+        DEV: "bg-[#bfadf4]",
+        TRENDING: "bg-[#fbabb6]",
+        MENTORSHIP: "bg-[#95f9ab]",
+    };
 
-    // const getButtonStyle = (category: string) => {
-    //     return currentSelection === category
-    //         ? "rounded-[66px] border border-[#333333] px-8 py-[21px] flex gap-4 justify-center items-center relative h-10 bg-white" // Selected style
-    //         : `rounded-[66px] border border-[#333333] px-8 py-[21px] flex gap-4 justify-center items-center relative h-10 ${filterStyles[currentSelection]}`; // Default style
-    // };
+    const getButtonStyle = useCallback(
+        (category: string) => {
+          return currentSelection === category
+            ? 'rounded-[66px] border border-[#333333] px-8 py-[21px] flex gap-4 justify-center items-center relative h-10 bg-white' // Selected style
+            : `rounded-[66px] border border-[#333333] px-8 py-[21px] flex gap-4 justify-center items-center relative h-10 ${filterStyles[category] || ''}`; // Default style
+        },
+        [currentSelection, filterStyles]
+      );
 
     return (
         <div
             className="flex justify-between items-center relative w-[999px] bg-transparent">
             <button
                 onClick={() => handleFilterClick('DESIGN')}
-                className="rounded-[66px] border border-[#333333] px-8 py-[21px] flex gap-4 justify-center items-center relative h-10 bg-[#ff9cf5]">
+                className={getButtonStyle('DESIGN')}>
                 <svg
                 width="24"
                 height="24"
@@ -58,14 +61,14 @@ const Filters: React.FC<FiltersProps> = ({ selectedCategory, onFilterChange }) =
 
             <button 
                 onClick={() => handleFilterClick('PRODUCT')}
-                className="rounded-[66px] border border-[#333333] px-8 py-[21px] flex gap-4 justify-center items-center relative w-[179px] h-10 bg-[#afbeff]">
+                className={getButtonStyle('PRODUCT')}>
                     <ProductFilterIcon/>
                     <h4 className="text-2xl text-[#333333]">Product</h4>
             </button>
             
             <button
                 onClick={() => handleFilterClick('DEV')}
-                className="rounded-[66px] border border-[#333333] px-8 py-[21px] flex gap-4 justify-center items-center relative h-10 bg-[#bfadf4]">
+                className={getButtonStyle('DEV')}>
                 <svg
                 width="31"
                 height="24"
@@ -81,7 +84,7 @@ const Filters: React.FC<FiltersProps> = ({ selectedCategory, onFilterChange }) =
 
             <button
                 onClick={() => handleFilterClick('TRENDING')}
-                className="rounded-[66px] border border-[#333333] px-8 py-[21px] flex gap-4 justify-center items-center relative h-10 bg-[#fbabb6]">
+                className={getButtonStyle('TRENDING')}>
                 <svg
                 width="25"
                 height="24"
@@ -108,7 +111,7 @@ const Filters: React.FC<FiltersProps> = ({ selectedCategory, onFilterChange }) =
 
             <button
                 onClick={() => handleFilterClick('MENTORSHIP')}
-                className="rounded-[66px] border border-[#333333] px-8 py-[21px] flex gap-4 justify-center items-center relative h-10 bg-[#95f9ab]">
+                className={getButtonStyle('MENTORSHIP')}>
                 <svg
                 width="24"
                 height="24"
