c4ce71cf27367c51ce3c4a7fb9a2a50a145f0bed
Nam Mai <maithenamanh@gmail.com>
2024-09-23 00:37:56 -0700

    Replaced svg in filter icons with icon components

---
+++ vtw-ui/src/components/icons/TrendingFilterIcon.tsx
@@ -0,0 +1,27 @@
+const TrendingFilterIcon = () => {
+    return (
+        <svg
+        width="25"
+        height="24"
+        viewBox="0 0 25 24"
+        fill="none"
+        xmlns="http://www.w3.org/2000/svg">
+        <g clipPath="url(#clip0_25_2694)">
+            <path
+            d="M1.99798 16.432L0.583984 15.018L7.65498 7.94696L14.019 14.311L18.262 10.068L16.519 8.32596L23.211 6.53296L21.418 13.225L19.676 11.483L14.019 17.139L7.65498 10.775L1.99798 16.432Z"
+            fill="#333333" />
+        </g>
+        <defs>
+            <clipPath id="clip0_25_2694">
+            <rect
+                width="24"
+                height="24"
+                fill="white"
+                transform="translate(0.583984)" />
+            </clipPath>
+        </defs>
+        </svg>
+    )
+}
+
+export default TrendingFilterIcon;
